98b7eeb75392271760886fe30b9953bad29a09f1
Алексей Тодорашко <149875136+LeshaTodorashko@users.noreply.github.com>
2024-03-09 12:48:47 +0200

    new old code

--- main.py
+++ main.py
@@ -1,5 +1,81 @@
 import pygame
+pygame.init()
 
-pygame.display.set_caption("ping_pong")
-window = pygame.display.set_mode((700, 500))
+win_w = 700
+win_h = 500
 
+FPS = 40
+
+window = pygame.display.set_mode((win_w, win_h))
+
+clock = pygame.time.Clock()
+
+background = pygame.image.load("flat.jpg")
+background = pygame.transform.scale(background, (win_w, win_h))
+
+class GameSprite:
+    def __init__(self, x, y, w, h, image):
+        self.rect = pygame.Rect(x, y, w, h)
+        image = pygame.transform.scale(image, (w, h))
+        self.image = image
+    
+    def update(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Pers(GameSprite):
+    def __init__(self, x, y, w, h, image, speed):
+        super().__init__(x, y, w, h, image)
+        self.speed = speed
+        self.clip = 5
+
+    def move(self, key_up, key_down):
+        k = pygame.key.get_pressed()
+        if k[key_down]:
+            if self.rect.right <= win_w:
+                self.rect.y += self.speed 
+        elif k[key_up]:
+            if self.rect.left >= 0:
+                self.rect.y -= self.speed
+        if self.rect.y <= 0:
+            self.rect.y += self.speed
+        elif self.rect.bottom >= 500:
+            self.rect.y -= self.speed
+
+class Ball(GameSprite):
+    def __init__(self, x, y, w, h, image, speed):
+        super().__init__(x, y, w, h, image)
+        self.speed = speed
+
+    def move(self):
+        self.rect.x += self.speed
+        self.rect.y += self.speed
+        
+
+raketka_img = pygame.image.load("Raketka.png")
+raketka1 = Pers(10, 200, 70, 60, raketka_img, 3)
+
+raketka2_img = pygame.image.load("Raketka.png")
+raketka2 = Pers(600, 200, 70, 60, raketka2_img, 3)
+
+ball_img = pygame.image.load("ball.png")
+ball = Ball(350, 250, 25, 25, ball_img, 4)
+
+game = True
+while game:
+    # window.fill((0, 255, 0))
+    window.blit(background, (0, 0))
+    raketka1.update()
+    raketka1.move(pygame.K_w, pygame.K_s)
+    raketka2.update()
+    raketka2.move(pygame.K_UP, pygame.K_DOWN)
+    ball.update()
+    ball.move()
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    pygame.display.update()
+    clock.tick(FPS)
